5b747c0a8b4d8d0481a88af5c81b45379b6aa989
Rajesh Kinjarapu <rajeshkinjarapu@gmail.com>
2026-09-02 11:31:20 +0530

    Fixed GatePassViewScreen missing file error

--- update_vps_version.bat
+++ update_vps_version.bat
@@ -2,14 +2,14 @@
 color 0A
 echo.
 echo ========================================================
-echo UPDATING APP VERSION ON VPS TO 1.0.2
+echo UPDATING APP VERSION ON VPS TO 1.0.3
 echo ========================================================
 echo.
 echo PLEASE ENTER VPS PASSWORD IF PROMPTED...
 echo.
-ssh -o StrictHostKeyChecking=no root@66.116.252.191 "echo {\"latestVersion\": \"1.0.2\", \"downloadUrl\": \"http://66.116.252.191:19998/app-release.apk\", \"forceUpdate\": false, \"releaseNotes\": \"Progress card full address, dynamic PDF filenames and new app names updated.\"} > /root/JY-School/backend/public/app-version.json && echo App version updated successfully!"
+ssh -o StrictHostKeyChecking=no root@66.116.252.191 "echo {\"latestVersion\": \"1.0.3\", \"downloadUrl\": \"https://github.com/rajeshkinjarapu/JY-School/actions\", \"forceUpdate\": false, \"releaseNotes\": \"Added Gatepass to Teacher Dashboard!\"} > /root/JY-School/backend/public/app-version.json && echo App version updated successfully!"
 echo.
 echo ========================================================
-echo DONE! The App Version has been updated to 1.0.2
+echo DONE! The App Version has been updated to 1.0.3
 echo ========================================================
 pause
